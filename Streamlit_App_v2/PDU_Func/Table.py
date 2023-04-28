8a25507fe20c881c815acb55899095b223fde679
M Irsyad Hibatullah <hibatullah.geophysics@gmail.com>
2023-04-28 14:24:38 +0700

    update agrid table on activitysummary

--- Streamlit_App_v2/PDU_Func/Table.py
+++ Streamlit_App_v2/PDU_Func/Table.py
@@ -149,7 +149,129 @@ def ActivityLogTable_Agrid(ActivityLog_DF, reload=False):
         reload_data =reload)
 	return InputActivityGrid_response
 
+def ActSumTable_Agrid(ActSumData, reload=False):
+    ActSum_DF = ActSumData.Data
+	string_to_add_row = ("\n\n function(e) { \n \
+    let api = e.api; \n \
+    let rowIndex = e.rowIndex + 1; \n \
+    let previousRowData = api.getDisplayedRowAtIndex(rowIndex - 1).data; \n \
+    let newRowData = Object.assign({}, previousRowData); \n \
+    api.applyTransaction({addIndex: rowIndex, add: [newRowData]}); \n \
+        }; \n \n")
+	cell_button_add = JsCode('''
+    class BtnAddCellRenderer {
+        init(params) {
+            this.params = params;
+            this.eGui = document.createElement('div');
+            this.eGui.innerHTML = `
+             <span>
+                <style>
+                .btn_add {
+                  background-color: limegreen;
+                  border: none;
+                  color: white;
+                  text-align: center;
+                  text-decoration: none;
+                  display: inline-block;
+                  font-size: 10px;
+                  height: 2.5em;
+                  width: 8em;
+                  cursor: pointer;
+                }
 
+                .btn_add :hover {
+                  background-color: #05d588;
+                }
+                </style>
+                <button id='click-button' 
+                    class="btn_add" 
+                    >&CirclePlus; Insert Row</button>
+             </span>
+          `;
+        }
+
+        getGui() {
+            return this.eGui;
+        }
+
+    };
+    ''')
+	string_to_delete =('''\n\n function(e) { \n \
+	    let api = e.api; \n \
+	    let sel = api.getSelectedRows(); \n \
+	    api.applyTransaction({remove: sel}); \n \
+	    };''')
+	cell_button_delete = JsCode('''
+	    class BtnCellRenderer {
+	        init(params) {
+	            console.log(params.api.getSelectedRows());
+	            this.params = params;
+	            this.eGui = document.createElement('div');
+	            this.eGui.innerHTML = `
+	             <span>
+	                <style>
+	                .btn {
+	                  background-color: #F94721;
+	                  border: none;
+	                  color: white;
+	                  font-size: 10px;
+	                  font-weight: bold;
+	                  height: 2.5em;
+	                  width: 8em;
+	                  cursor: pointer;
+	                }
+
+	                .btn:hover {
+	                  background-color: #FB6747;
+	                }
+	                </style>
+	                <button id='click-button'
+	                    class="btn"
+	                    >&#128465; Delete</button>
+	             </span>
+	          `;
+	        }
+
+	        getGui() {
+	            return this.eGui;
+	        }
+
+	    };
+	    ''')
+
+	gridOptions  = GridOptionsBuilder.from_dataframe(ActSum_DF.drop(['id','DateTime'], 1))
+	# gridOptions  = GridOptionsBuilder.from_dataframe(ActivityLog_DF.drop(['DateTime'], 1))
+	gridOptions.configure_selection('single')
+
+    #TODO
+    # column edit on SubActivity
+    # column edit on datetime end
+    # column edit on remarks
+    # gridOptions.configure_column('SubActivity',editable=True)
+    # gridOptions.configure_column('End DateTime',editable=True)
+    # gridOptions.configure_column('remarks',editable=True)
+
+
+
+	gridOptions.configure_column('Insert', headerTooltip='Click on Button to add new row', editable=False, filter=False,
+                            onCellClicked=JsCode(string_to_add_row), cellRenderer=cell_button_add,
+                            autoHeight=True, wrapText=True, suppressMovable='true')
+	gridOptions.configure_column('Delete', headerTooltip='Click on Button to remove row',
+                                    editable=False, filter=False, onCellClicked=JsCode(string_to_delete),
+                                    cellRenderer=cell_button_delete,
+                                    autoHeight=True, )
+	gridOptions.configure_default_column(editable=False)
+	gb = gridOptions.build()
+	
+	InputActivityGrid_response = AgGrid(
+		ActivityLog_DF, 
+        gridOptions=gb,
+        data_return_mode=DataReturnMode.FILTERED_AND_SORTED, 
+        # update_mode=(GridUpdateMode.NO_UPDATE),
+        update_mode=(GridUpdateMode.VALUE_CHANGED) | (GridUpdateMode.SELECTION_CHANGED) ,
+	    allow_unsafe_jscode=True,
+        reload_data =reload)
+	return InputActivityGrid_response
 # df=pd.DataFrame({ "Name": ['Erica', 'Rogers', 'Malcolm', 'Barrett'], "Age": [43, 35, 57, 29]})
 
 
