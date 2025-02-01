215594996f67f6c96293138a119de1d6510ee535
pdu root <root@pdumitradome.id>
2025-02-02 03:56:20 +0800

    separate config.toml and add ssl certificate

--- setup.sh
+++ setup.sh
@@ -4,10 +4,12 @@ echo "[server]
 headless = true
 port = $PORT
 enableCORS = false
+sslCertFile = 'SSL/ssl.pdumitradome.id.pem'
+sslKeyFile = 'SSL/ssl.pdumitradome.id.key'
 [theme]
 base = 'light'
 [client]
 showErrorDetails=false
-sslCertFile = '/SSL/ssl.pdumitradome.id.pem'
-sslKeyFile = 'SSL/ssl.pdumitradome.id.key'
+
+
 " > ~/.streamlit/config.toml
