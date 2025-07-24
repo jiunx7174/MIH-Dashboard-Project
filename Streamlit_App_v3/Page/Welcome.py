398bc40c21d538d1d6cd2167f6f4279d742c649a
pdu root <root@pdumitradome.id>
2025-07-24 22:22:26 +0800

    update lottie homepage

--- Streamlit_App_v3/Page/Welcome.py
+++ Streamlit_App_v3/Page/Welcome.py
@@ -4,7 +4,9 @@ import requests
 import json
 
 def App():
-    lottie_json = json.loads(requests.get("https://assets4.lottiefiles.com/packages/lf20_ccvekmzu.json").text)
+    with open(r'Data/lottie-files/WelcomeLottie.json', 'r') as f:
+    # lottie_json = json.loads(requests.get("https://assets4.lottiefiles.com/packages/lf20_ccvekmzu.json").text)
+        lottie_json = json.load(f)
     col_temp = st.columns(3)
     with col_temp[1]:
         st_lottie(lottie_json, width=420, speed=1,key="hello")
